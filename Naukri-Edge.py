0350b512d97bf4bf7155ef68a97aa1db02cfcc76
kaustubh319 <kaustubhupadhya@gmail.com>
2025-07-21 19:51:33 +0530

    Update Naukri-Edge.py

--- Naukri-Edge.py
+++ Naukri-Edge.py
@@ -6,6 +6,7 @@ import random
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.edge.service import Service as EdgeService
+from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException, NoSuchElementException, ElementClickInterceptedException
 from bs4 import BeautifulSoup
